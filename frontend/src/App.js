c01ab0b5087b8ce65df13c3b2294fe5f2b827889
carlos <carlos7legend@gmail.com>
2020-02-22 13:39:52 -0800

    init react app

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import LeftDrawer from './Components/Drawer/LeftDrawer'
+
+import Grid from '@material-ui/core/Grid';
+
+
+function App() {
+  return (
+    <Grid>
+      <LeftDrawer></LeftDrawer>
+
+    </Grid>
+  );
+}
+
+export default App;
